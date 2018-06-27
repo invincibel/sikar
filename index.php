12430939f913de671667b89af7e45f49a2f2ea10
invincibel <31722976+invincibel@users.noreply.github.com>
2018-06-27 23:06:21 +0530

    updated

--- index.php
+++ index.php
@@ -4,7 +4,45 @@
 <div id="main">
     <span class="middle" id="demo"></span>
 </div>
-
+<div id="content">
+    <h1 class="text" id="tex3">Get The latest movies here</h1>
+</div>
+<div class="container-fluid">
+   
+      <div class="row">
+     
+      <div class="col-md-3">
+         <div class="col1"></div>
+      </div>
+      <div class="col-md-6">
+         <div class="col2">
+           
+         </div>
+      </div>
+      <div class="col-md-3">
+         <div class="col3"></div>
+      </div>
+        </div>
+     
+      <div class="row">
+      <div class="col-md-3">
+         <div class="col4"></div>
+      </div>
+      <div class="col-md-3">
+         <div class="col5"></div>
+      </div>
+      <div class="col-md-3">
+         <div class="col6"></div>
+      </div>
+        <div class="col-md-3">
+         <div class="col7"></div>
+      </div>
+     
+   
+      </div>
+       
+    </div>
+<br><br>
 <script src="scripts/main.js"></script>
 <?php
 include 'footer.html';
