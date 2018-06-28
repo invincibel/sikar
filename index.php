75603f1567afb32381750cef80a3c0bc068c0fc9
rahul malhotra <rahul.ma1702@gmail.com>
2018-06-28 20:46:13 +0530

    updated
    updated

--- index.php
+++ index.php
@@ -1,8 +1,163 @@
 <?php
  include 'header.html';
 ?>
-<div class="container">
+<div class="parallax">
+<div class="new">
+    <html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+  .parallax { 
+    /* The image used */
+    background-image: url("pexels-photo-11744.jpeg");
+
+    /* Set a specific height */
+    height: 500px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+div.gallery {
+    margin: 60px;
+    border: 1px solid #ccc;
+    float: left;
+    width: 180px;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="gallery">
+  <a target="_blank" href="fjords.jpg">
+    <img src="download.png" alt="Cinque Terre" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.jpg">
+    <img src="image3.jpg" alt="Forest" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image2.png">
+    <img src="image2.png" alt="Northern Lights" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.jpg">
+    <img src="image1.jpg" alt="Mountains" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+    <div class="gallery">
+  <a target="_blank" href="fjords.jpg">
+    <img src="download.png" alt="Cinque Terre" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.jpg">
+    <img src="image3.jpg" alt="Forest" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image2.png">
+    <img src="image2.png" alt="Northern Lights" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.jpg">
+    <img src="image1.jpg" alt="Mountains" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+    <div class="gallery">
+  <a target="_blank" href="fjords.jpg">
+    <img src="download.png" alt="Cinque Terre" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.jpg">
+    <img src="image3.jpg" alt="Forest" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image2.png">
+    <img src="image2.png" alt="Northern Lights" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.jpg">
+    <img src="image1.jpg" alt="Mountains" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+    <div class="gallery">
+  <a target="_blank" href="fjords.jpg">
+    <img src="download.png" alt="Cinque Terre" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.jpg">
+    <img src="image3.jpg" alt="Forest" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image2.png">
+    <img src="image2.png" alt="Northern Lights" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.jpg">
+    <img src="image1.jpg" alt="Mountains" width="400" height="300">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
     
+
+</body>
+</html>
+</div>
 </div>
 <?php
 include 'footer.html';
