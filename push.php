53b22e84e779e43015e8fa1dc8b66442d3ea6f68
invincibel <31722976+invincibel@users.noreply.github.com>
2018-07-02 21:38:49 +0530

    updated

--- push.php
+++ push.php
@@ -3,10 +3,21 @@
  $a = $_POST['email'];
 $b = $_POST['movie'];
 if(strlen($a)==0 || strlen($b)==0)
+{
+    echo 'cannot be null';
+  header("refresh:1;url=index.php");
     return;
+}
 if (!filter_var($a, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format"; 
       echo $emailErr;
       header("refresh:1;url=index.php");
+    return;
     }
+  $q ="INSERT INTO `data`(`email`, `movie`) VALUES ('$a','$b');";
+   $query = mysqli_query($hritik,$q);
+if($query){
+    echo 'you will be notify when movie is available';
+    header("refresh:1;url=index.php");
+}
 ?>
